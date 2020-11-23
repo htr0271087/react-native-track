990a4049921a227e062c45fc25014bf9a2518fb9
Linh Tran <htr0271087@gmail.com>
2020-11-23 12:23:07 +0700

    finish sign in / up

---
+++ track-server/src/index.js
@@ -0,0 +1,33 @@
+require('./models/User');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require("./routes/authRoutes");
+const requireAuth = require("./middleware/requireAuth");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(authRoutes);
+
+const mongoUri = "mongodb+srv://admin:Password@123@track-server.rdylm.mongodb.net/track-server?retryWrites=true&w=majority";
+
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+});
+
+mongoose.connection.on("connected", () => {
+    console.log("Connected to mongo instance");
+});
+
+mongoose.connection.on("error", (err) => {
+    console.log('Error connecting to mongo', err);
+});
+
+app.get("/", requireAuth, (req, res) => {
+    res.send(`You email: ${req.user.email}`);
+});
+
+app.listen(3000, () => {
+    console.log("Listen on port 3000");
+});
